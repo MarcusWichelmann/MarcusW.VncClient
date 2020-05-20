f209708d8b0b28695eee2d51501049998aecf093
Marcus Wichelmann <marcus.wichelmann@hotmail.de>
2020-05-20 19:12:07 +0200

    Documentation improvements

--- src/MarcusW.VncClient/VncDefaults.cs
+++ src/MarcusW.VncClient/VncDefaults.cs
@@ -3,8 +3,16 @@ using MarcusW.VncClient.Protocol.Encodings;
 
 namespace MarcusW.VncClient
 {
+    /// <summary>
+    /// Provides default values for VNC Client options.
+    /// </summary>
     public static class VncDefaults
     {
+        /// <summary>
+        /// Builds a collection with all RFB encodings that are officially supported by this library.
+        /// Feel free to extend the returned enumerable with custom encodings.
+        /// </summary>
+        /// <returns>The encoding collection.</returns>
         public static IEnumerable<IEncoding> GetEncodingsCollection()
         {
             // TODO: Add encodings
